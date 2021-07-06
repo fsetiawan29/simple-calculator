b219067bbd574ac9e3f1cb5ddc5841a1ce3bc3da
Febryan Setiawan <febryan.setiawan@tokopedia.com>
2021-07-06 13:11:55 +0700

    Add select operation

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
+		fmt.Println("List Operation")
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Printf("Select Operation: ")
+		scanner.Scan()
+		operation := scanner.Text()
+
 		fmt.Printf("Enter first value: ")
 		scanner.Scan()
 		firstValue := scanner.Text()
@@ -30,7 +39,16 @@ func main() {
 			break
 		}
 
-		result := module.Add(firstValueInt, secondValueInt)
+		var result int
+		if operation == "1" {
+			result = module.Add(firstValueInt, secondValueInt)
+		} else if operation == "2" {
+			result = module.Subtract(firstValueInt, secondValueInt)
+		} else if operation == "3" {
+			result = module.Multiply(firstValueInt, secondValueInt)
+		} else if operation == "4" {
+			result = module.Divide(firstValueInt, secondValueInt)
+		}
 
 		fmt.Printf("The result is %d\n", result)
 
